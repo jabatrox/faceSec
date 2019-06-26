fd2fe61306f651442f28048057c6c9ca1dba5157
Javier Soler <jsoler92@hotmail.com>
2019-06-26 10:20:37 -0500

    Fix: HTML template and JavaScript - Fix: HTML template now loads local JavaScript libraries - Fix: JavaScript running is now on 'app.js' file

---
+++ static/js/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    // Display the default welcome message on page load
+    window.onload = function(){
+        $('#welcome').html("<p>Waiting for user...</p>");
+    };
+
+    // Connect to the socket server
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+    var connect_error_counter = 0;
+    // Receive new welcome message from server
+    socket.on('newMessage', function(msg) {
+        console.log("Welcome message = '" +msg.message+ "'");
+        // Update the welcome message to be displayed
+        welcome_message = "<p>" + msg.message.toString() + "</p>"
+        $('#welcome').html(welcome_message);
+        // socket.emit('newMessage');
+    });
+    socket.emit('newMessage');
+    socket.on('connect_error', function(err) {
+        // If there are 3 connection errors to the server, close the
+        // socket
+        console.log('[WARNING] Error connecting to server');
+        connect_error_counter++;
+
+        if (connect_error_counter == 3) {
+            console.log('[ERROR] Server unreachable, closing socket...');
+            socket.disconnect();
+            console.log('[DONE] Socket closed');
+        }
+    });
+
+});
+// window.onbeforeunload = function() {
+//     websocket.onclose = function () {}; // disable onclose handler first
+//     websocket.close();
+// };
